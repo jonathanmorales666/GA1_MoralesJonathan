a4eaf670cdca447d867b1d86270f168ecc705533
jonapapayero <jonathanmorales@emilianisomascos.edu.gt>
2026-04-15 10:21:22 -0600

    Create Habitat class with constructor and describirClima method

---
+++ habitat.java
@@ -0,0 +1,13 @@
+public class Habitat {
+    private String nombre;
+    private double area;
+
+    public Habitat(String nombre, double area) {
+        this.nombre = nombre;
+        this.area = area;
+    }
+
+    public void describirClima() {
+        // Method implementation goes here
+    }
+}
